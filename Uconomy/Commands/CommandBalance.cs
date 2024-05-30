8cf3cffbb2d65a141a83b5296b5d33f966e91eb2
Tavstal <tavstal@gmail.com>
2024-05-30 10:35:03 +0200

    Upgraded balance command

--- Uconomy/Commands/CommandBalance.cs
+++ Uconomy/Commands/CommandBalance.cs
@@ -1,4 +1,5 @@
 ﻿using Rocket.API;
+using Rocket.Unturned.Player;
 using System.Collections.Generic;
 using Tavstal.TLibrary.Utils;
 
@@ -6,17 +7,38 @@ namespace fr34kyn01535.Uconomy
 {
     public class CommandBalance : IRocketCommand
     {
-        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
         public string Name => "balance";
         public string Help => "Shows the current balance";
-        public string Syntax => "";
+        public string Syntax => "<player>";
         public List<string> Aliases => new List<string> { "saldo" };
-        public List<string> Permissions => new List<string> { "uconomy.balance", "uconomy.commands.balance" };
+        public List<string> Permissions => new List<string> { "uconomy.balance", "uconomy.commands.balance", "uconomy.commands.balance.other" };
 
         public void Execute(IRocketPlayer caller, params string[] command)
         {
-            decimal balance = Uconomy.Instance.Database.GetBalance(caller.Id);
-            ChatHelper.SendCommandReply(caller,"command_balance_show", balance, Uconomy.Instance.Configuration.Instance.MoneyName);
+            if (command.Length == 0)
+            {
+                decimal balance = Uconomy.Instance.Database.GetBalance(caller.Id);
+                ChatHelper.SendCommandReply(caller, "command_balance_show", balance, Uconomy.Instance.Configuration.Instance.MoneyName);
+                return;
+            }
+
+            if (!caller.HasPermission("uconomy.commands.balance.other"))
+            {
+                ChatHelper.SendCommandReply(caller, "command_balance_other_forbidden");
+                return;
+            }
+
+            UnturnedPlayer target = UnturnedPlayer.FromName(command[0]);
+            if (target == null)
+            {
+                ChatHelper.SendCommandReply(caller, "command_pay_error_player_not_found");
+                return;
+            }
+
+            decimal targetBalance = Uconomy.Instance.Database.GetBalance(target.Id);
+
+            ChatHelper.SendCommandReply(caller, "command_balance_show_other", targetBalance, Uconomy.Instance.Configuration.Instance.MoneyName, target.CharacterName);
         }
     }
 }
