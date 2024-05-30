e0d2dcb162e14b0b4364c046115093f72cf23c9a
Tavstal <tavstal@gmail.com>
2024-05-30 10:35:34 +0200

    Added models

---
+++ Uconomy/Models/DeathPenalty.cs
@@ -0,0 +1,17 @@
+﻿using SDG.Unturned;
+using System;
+
+namespace fr34kyn01535.Uconomy.Models
+{
+    [Serializable]
+    public class DeathPenalty
+    {
+        public EDeathCause Cause { get; set; }
+        public bool Enable { get; set; }
+        public decimal Fine { get; set; }
+
+        public DeathPenalty(EDeathCause cause, bool enable, decimal fine) { Cause = cause; Enable = enable; Fine = fine; }
+
+        public DeathPenalty() { }
+    }
+}
---
+++ Uconomy/Models/KillReward.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace fr34kyn01535.Uconomy.Models
+{
+    [Serializable]
+    public class KillReward
+    {
+        public bool Enable { get; set; }
+        public string EventName { get; set; }
+        public decimal Reward { get; set; }
+
+        public KillReward(string eventName, decimal reward, bool enable) { EventName = eventName; Reward = reward; Enable = enable; }
+
+        public KillReward() { }
+    }
+}
