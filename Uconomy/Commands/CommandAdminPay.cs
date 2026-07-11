07db597a1669766d83d9b6f961339d781a93b5af
tavstal <tavstal@gmail.com>
2026-07-11 20:55:48 +0200

    Add XML documentation to CommandAdminPay class and improve command response handling

--- Uconomy/Commands/CommandAdminPay.cs
+++ Uconomy/Commands/CommandAdminPay.cs
@@ -1,11 +1,16 @@
 ﻿using Rocket.API;
 using Rocket.Unturned.Player;
 using System.Collections.Generic;
-using Tavstal.TLibrary.Utils;
+using fr34kyn01535.Uconomy.Utils;
+// ReSharper disable UnusedType.Global
 
 namespace fr34kyn01535.Uconomy.Commands
 {
-    internal class CommandAdminPay
+    /// <summary>
+    /// Admin command that adds money directly to a player's balance.
+    /// Unlike the regular pay command, this does not deduct from the caller's account.
+    /// </summary>
+    public class CommandAdminPay : IRocketCommand
     {
         public AllowedCaller AllowedCaller => AllowedCaller.Both;
         public string Name => "adminpay";
@@ -14,38 +19,43 @@ namespace fr34kyn01535.Uconomy.Commands
         public List<string> Aliases => new List<string> { "apay" };
         public List<string> Permissions => new List<string> { "uconomy.adminpay", "uconomy.commands.adminpay" };
 
+        /// <summary>
+        /// Executes the admin pay command, adding the specified amount
+        /// to the target player's balance.
+        /// </summary>
+        /// <param name="caller">The admin or console executing the command.</param>
+        /// <param name="command">
+        /// An array where:
+        /// [0] is the name of the player to pay,
+        /// [1] is the amount of money to add.
+        /// </param>
         public void Execute(IRocketPlayer caller, params string[] command)
         {
             if (command.Length != 2)
             {
-                ChatHelper.SendCommandReply(caller, "command_pay_invalid");
+                CommandUtils.SendCommandReply(caller, Uconomy.Instance.Translate("command_adminpay_error_invalid", Uconomy.Instance.GetPrefix(), Syntax));
                 return;
             }
 
             UnturnedPlayer otherPlayer = UnturnedPlayer.FromName(command[0]);
             if (otherPlayer != null)
             {
-                if (caller == otherPlayer)
+                if (!decimal.TryParse(command[1], out var amount) || amount <= 0)
                 {
-                    ChatHelper.SendCommandReply(caller, "command_pay_error_pay_self");
-                    return;
-                }
-
-                decimal amount = 0;
-                if (!decimal.TryParse(command[1], out amount) || amount <= 0)
-                {
-                    ChatHelper.SendCommandReply(caller, "command_pay_error_invalid_amount");
+                    CommandUtils.SendCommandReply(caller, Uconomy.Instance.Translate("command_pay_error_invalid_amount", Uconomy.Instance.GetPrefix()));
                     return;
                 }
 
                 Uconomy.Instance.Database.IncreaseBalance(otherPlayer.Id, amount);
-                ChatHelper.SendCommandReply(caller, "command_pay_private", otherPlayer.CharacterName, amount, Uconomy.Instance.Configuration.Instance.MoneyName);
-                ChatHelper.SendCommandReply(otherPlayer, "command_pay_console", amount, Uconomy.Instance.Configuration.Instance.MoneyName);
+                CommandUtils.SendCommandReply(caller, Uconomy.Instance.Translate("command_pay_private", Uconomy.Instance.GetPrefix(), otherPlayer.CharacterName, 
+                    amount, Uconomy.Instance.Configuration.Instance.MoneyName));
+
+                CommandUtils.SendCommandReply(otherPlayer, Uconomy.Instance.Translate("command_pay_console", Uconomy.Instance.GetPrefix(), amount, 
+                    Uconomy.Instance.Configuration.Instance.MoneyName));
+                return;
             }
-            else
-            {
-                ChatHelper.SendCommandReply(caller, "command_pay_error_player_not_found");
-            }
+
+            CommandUtils.SendCommandReply(caller, Uconomy.Instance.Translate("command_pay_error_player_not_found", Uconomy.Instance.GetPrefix()));
         }
     }
 }
